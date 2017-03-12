8751ec416aa397df394cc417b950ac290d670185
Kyle Francis Boclot <Kyle Francis Boclot>
2017-03-12 17:19:19 +0800

    upload

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,12 +8,12 @@ class AccountsController < ApplicationController
 	    @accounts.name = params[:account][:name]
 	    @accounts.email = params[:account][:email]
 	    @accounts.age = params[:account][:age]
-	    @accounts.department = params[:account][:department]
-	    @accounts.college = params[:account][:college]
+	    @accounts.department = params[:account][:department] #ini
+	    @accounts.college = params[:account][:college] #dawa ini
 	    @accounts.password = params[:account][:password]
-	    @accounts.repassword = params[:account][:repassword]
-	    @accounts.save
-	    redirect_to "/accounts/#{@accounts.id}"
+	    @accounts.repassword = params[:account][:repassword] #pwede man lang baga ni params[:repassword]
+	    @accounts.sav
+	    redirect_to "/accounts/#{@accounts.id}" 
 	end
 
 	def profile
